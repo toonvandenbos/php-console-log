a0168c1e1b811749027e8beab1e6e821dbd98468
Toon Van den Bos <toon@whitecube.be>
2016-05-25 18:48:54 +0200

    Added example section about setAttribute

--- examples/index.php
+++ examples/index.php
@@ -40,7 +40,7 @@ require_once(__DIR__ . '/../php-console-log.php');
             </div>
             <div class="example__code">
                   <pre class="example__format">
-// require the library
+<em>// require the library</em>
 
 require_once(__DIR__ . '/../php-console-log.php');</pre>
             </div>
@@ -49,35 +49,81 @@ require_once(__DIR__ . '/../php-console-log.php');</pre>
             </div>
             <div class="example__code">
                   <pre class="example__format">
-&lt;!-- [...] some templating code --&gt;
+<em>&lt;!-- [...] some templating code --&gt;</em>
 
 &lt;?php <strong>execConsoleLogs();</strong> ?&gt;
 
-&lt;!-- [...] some other templating code --&gt;</pre>
+<em>&lt;!-- [...] some other templating code --&gt;</em></pre>
             </div>
             <div class="example__text">
                   <p>Good. We're now all set. You can start using the consoleLog() everywhere, as long as it is <strong>called before an <em>execConsoleLogs()</em></strong> call.</p>
             </div>
             <div class="example__code">
                   <pre class="example__format">
-// [...] some templating/PHP code
+<em>// [...] some templating/PHP code</em>
 
 function handleSomeVar( $data ){
-      // ... do stuff.
+      <em>// ... do stuff.</em>
       <strong>consoleLog($data);</strong>
-      // ... do some other stuff
+      <em>// ... do some other stuff</em>
 }
 
-// [...] some templating/PHP code</pre>
+<em>// [...] some templating/PHP code</em></pre>
             </div>
             <div class="example__text">
                   <p>Take a look at your console, you should see what this basic consoleLog outputed on this page!</p>
             </div>
+
             <?php 
                   // Output a simple var in the browser's console :
                   $myVar = '#1 - Hello! This is example n.1 - How are you? :)';
                   consoleLog( $myVar );
             ?>
+            
+      </section>
+
+      <section class="example">
+            <h2 class="example__title">#2 - The script tag</h2>
+            <div class="example__text">
+                  <p>All this library does is generating an HTML <em>&lt;script&gt;</em> tag where you asked it (using the <em>execConsoleLogs()</em> function).</p>
+                  <p>This tag contains the required JavaScript logic in order to output the requested data in the browser's console. There is not much you could do on the generated JavaScript code, but you could want to add an attribute on the <em>&lt;script&gt;</em> tag.</p>
+                  <p>By default, the library generates the following HTML markup:</p>
+            </div>
+            <div class="example__code">
+                  <pre class="example__format">&lt;script type="text/javascript" data-php-console-log="true"&gt;<em>[...]</em>&lt;/script&gt;</pre>
+            </div>
+            <div class="example__text">
+                  <p>You can remove or modify the <em>data-php-console-log</em> attribute by setting it as follows:</p>
+            </div>
+            <div class="example__code">
+                  <pre class="example__format">
+/*
+ * Remove the attribute :
+ */
+
+$PHPConsoleLog->setAttribute();
+<em>// or</em>
+$PHPConsoleLog->setAttribute(false);
+
+
+/*
+ * Edit the attribute :
+ */
+
+<em>// simple attribute</em>
+$PHPConsoleLog->setAttribute('data-my-attribute');
+<em>// attribute with value</em>
+$PHPConsoleLog->setAttribute('data-my-attribute', 'attribute-value');</pre>
+            </div>
+            <div class="example__text">
+                  <p>On this page, we modified the attribute to <em>data-hello="Is there anybody?"</em>. Inspect the code, and take a look at the <em>&lt;script&gt;</em> tag, at the end of this file, where <em>execConsoleLogs()</em> was called (just before the <em>&lt;/body&gt;</em> tag).</p>
+            </div>
+
+            <?php 
+                  // Change the script tag attribute
+                  $PHPConsoleLog->setAttribute('data-hello', 'Is there anybody?');
+            ?>
+
       </section>
 
       <footer class="main-footer">

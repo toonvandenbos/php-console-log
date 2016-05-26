929f8649590afd425ca232247761e41bedba6bf0
Toon Van den Bos <toon@whitecube.be>
2016-05-26 12:40:44 +0200

    Updated example copy with new Console::log() syntax.

--- examples/index.php
+++ examples/index.php
@@ -34,30 +34,34 @@ use PHPConsoleLog\Service as Console;
             <a href="https://github.com/nyratas/php-console-log" class="main-head__cta">Back to GitHub</a>
       </header>
 
+
+
       <section class="example">
             <h2 class="example__title">#1 - Basic PHP console.log()</h2>
             <div class="example__text">
-                  <p>Start by <strong>requiring</strong> the library.</p>
+                  <p>Start by <strong>requiring</strong> and <strong>using</strong> the library.</p>
             </div>
             <div class="example__code">
                   <pre class="example__format">
 <em>// require the library</em>
 
-require_once(__DIR__ . '/../php-console-log.php');</pre>
+require_once(__DIR__ . '/../src/PHPConsoleLog/__autoload.php');
+
+use PHPConsoleLog\Service as Console;</pre>
             </div>
             <div class="example__text">
-                  <p>Next, you should <strong>define a place where the library should output</strong> the console logs:</p>
+                  <p>Next, you should define a place where you want to output the console.logs:</p>
             </div>
             <div class="example__code">
                   <pre class="example__format">
 <em>&lt;!-- [...] some templating code --&gt;</em>
 
-&lt;?php <strong>execConsoleLogs();</strong> ?&gt;
+&lt;?php <strong>Console::exec();</strong> ?&gt;
 
 <em>&lt;!-- [...] some other templating code --&gt;</em></pre>
             </div>
             <div class="example__text">
-                  <p>Good. We're now all set. You can start using the consoleLog() everywhere, as long as it is <strong>called before an <em>execConsoleLogs()</em></strong> call.</p>
+                  <p>Good. We're now all set. You can start using the Console::log() everywhere, as long as it is <strong>called before a <em>Console::exec()</em></strong> call.</p>
             </div>
             <div class="example__code">
                   <pre class="example__format">
@@ -65,20 +69,20 @@ require_once(__DIR__ . '/../php-console-log.php');</pre>
 
 function handleSomeVar( $data ){
       <em>// ... do stuff.</em>
-      <strong>consoleLog($data);</strong>
+      <strong>Console::log($data);</strong>
       <em>// ... do some other stuff</em>
 }
 
 <em>// [...] some templating/PHP code</em></pre>
             </div>
             <div class="example__text">
-                  <p>You can use <strong>consoleLog()</strong> with any type of variable. If you want to output a PHP object, the library will show a JavaScript Object in the browser's console. A boolean will remain a boolean, an integer will remain an integer, a string remain a string, and so on.</p>
-                  <p>Take a look at your console, you should see what this basic consoleLog outputed on this page!</p>
+                  <p>You can use <strong>Console::log()</strong> with any type of variable. If you want to output a PHP array or object, the library will show a JavaScript Array/Object in the browser's console. A boolean will remain a boolean, an integer will remain an integer, a string remain a string, and so on.</p>
+                  <p>Take a look at your console, you should see what this basic Console::log() outputed on this page!</p>
             </div>
 
             <?php 
                   // Output a simple var in the browser's console :
-                  Console::log( '#1 - Hello! This is example n.1 - The following object once was a simple PHP\'s stdClass(), but consoleLog formatted it so javascript can display it.' );
+                  Console::log( '#1 - Hello! This is example n.1 - The following object once was a simple PHP\'s stdClass(), but Console::log() formatted it so javascript can display it correctly.' );
                   $myVar = new stdClass();
                   $myVar->string = "hello";
                   $myVar->boolean = true;
@@ -95,9 +99,9 @@ function handleSomeVar( $data ){
       <section class="example">
             <h2 class="example__title">#2 - The script tag</h2>
             <div class="example__text">
-                  <p>All this library does is generating an HTML <em>&lt;script&gt;</em> tag where you asked it (using the <em>execConsoleLogs()</em> function).</p>
-                  <p>This tag contains the required JavaScript logic in order to output the requested data in the browser's console. There is not much you could do on the generated JavaScript code, but you could want to add an attribute on the <em>&lt;script&gt;</em> tag.</p>
-                  <p>By default, the library generates the following HTML markup:</p>
+                  <p>All this library does is generating an HTML <strong>&lt;script&gt;</strong> tag.</p>
+                  <p>This tag contains the required JavaScript logic in order to output the requested data in the browser's console. There is not much you can do on the generated JavaScript code, but you could want to add an attribute on the <strong>&lt;script&gt;</strong> tag.</p>
+                  <p>The library generates the following default HTML markup:</p>
             </div>
             <div class="example__code">
                   <pre class="example__format">&lt;script type="text/javascript" data-php-console-log="true"&gt;<em>[...]</em>&lt;/script&gt;</pre>
@@ -111,9 +115,9 @@ function handleSomeVar( $data ){
  * Remove the attribute :
  */
 
-$PHPConsoleLog->setAttribute();
+Console::setAttribute();
 <em>// or</em>
-$PHPConsoleLog->setAttribute(false);
+Console::setAttribute(false);
 
 
 /*
@@ -121,12 +125,12 @@ $PHPConsoleLog->setAttribute(false);
  */
 
 <em>// simple attribute</em>
-$PHPConsoleLog->setAttribute('data-my-attribute');
+Console::setAttribute('data-my-attribute');
 <em>// attribute with value</em>
-$PHPConsoleLog->setAttribute('data-my-attribute', 'attribute-value');</pre>
+Console::setAttribute('data-my-attribute','attribute-value');</pre>
             </div>
             <div class="example__text">
-                  <p>On this page, we modified the attribute to <em>data-hello="Is there anybody?"</em>. Inspect the code, and take a look at the <em>&lt;script&gt;</em> tag, at the end of this file, where <em>execConsoleLogs()</em> was called (just before the <em>&lt;/body&gt;</em> tag).</p>
+                  <p>On this page, we modified the attribute to <em>data-hello="Is there anybody?"</em>. Inspect the code, and take a look at the <strong>&lt;script&gt;</strong> tag, at the end of this file, where <strong>Console::exec()</strong> was called (just before the <strong>&lt;/body&gt;</strong> tag).</p>
             </div>
 
             <?php 
